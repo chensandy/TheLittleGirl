74265a6b2bfeb7884f5e691a2381e717e7ec615b
chensandy <chensandy99@hotmail.com>
2015-06-22 02:26:12 +0800

    door音效 & map talk

--- The Little Girl/door.lua
+++ The Little Girl/door.lua
@@ -7,14 +7,14 @@ function door_load() --15
 			   6,
 			   8, 8, 8, --3
 			   10, 10, --2
-			   11}
+			   11, 3}
 	doorId = {2, 4, 3, 1,
 			  1, 2, 3, 4, 5, 7,
 			  1,
 			  1,
 			  1, 2, 3,
 			  1, 2,
-			  1}
+			  1, 6}
 --[[
 	doorLock = {1, 0, 1, 1,
 				0, 1, 1, 0, 1, 1,
@@ -22,7 +22,7 @@ function door_load() --15
 				0,--1,
 				0, 0, 1,
 				0, 0,
-				1}
+				1, 1}
 --]]
 	doorLock = {0, 0, 0, 1,
 				0, 0, 0, 0, 0, 0,
@@ -30,7 +30,7 @@ function door_load() --15
 				0,
 				0, 0, 0,
 				0, 0,
-				0}
+				0, 1}
 --]]
 	doorKey = {"Items/2-1-1.png", 0, "Items/5-1-1-1.png", -1,
 			   0, "Items/4-2-1.png", "Items/10-1-4-1.png", 0, "Items/9-1-1-1.png", "Items/10-2-1-4.png",
@@ -38,49 +38,49 @@ function door_load() --15
 			   0,
 			   0, 0, "Items/7-3-1.png",
 			   0, 0,
-			   "Items/10-2-2-2.png"}
+			   "Items/10-2-2-2.png", 0}
 	doorGo = {3, 2, 8, 0,
 			  4, 5, 6, 1, 7, 13,
 			  5,
 			  6,
 			  1, 9, 10,
 			  8, 11,
-			  11}
+			  11, 0}
 	doorGoBranch = {1, 0, 1, 0,
 					0, 0, 0, 0, 0, 0,
 					3,
 					2,
 					0, 0, 2,
 					2, 0,
-					3}
+					3, 0}
 	doorOpenTalk = {"使用鑰匙，門鎖被打開了。", "", "使用磁卡，電子門打開了。", "",
 					"", "使用鑰匙，馬克教授的門鎖開了。", "使用鑰匙，麥斯副教授的門鎖開了。", "", "利用項鍊機關中彈出的鑰匙，門鎖開了。", "使用鑰匙，門鎖被打開了。",
 					"插入木棒後，好像撬開了什麼東西，抽屜打開了。",
 					"",
 					"", "", "使用磁卡，電子門打開了。",
 					"", "",
-					"把寶箱的鎖頭打開了。"}
+					"把寶箱的鎖頭打開了。",""}
 	doorTalk = {"鎖著的，不知道是會通往哪裡的門。", "", "一個不知道通往哪裡的電子門，旁邊有感應區塊，要怎麼進去呢？", "我進來這裡的門，沒有任何能開啟的跡象，看來得從其他地方想辦法出去才行。",
 				"", "鎖著的房門，上面寫著馬克教授。", "鎖著的房門，上面寫著麥斯副教授。", "", "鎖著的房門，上面寫著伊莉以及愛洛研究員。", "鎖著的，打不開。",
 				"有一個洞。",
 				"",
 				"", "", "電子門，看來不能使用之前的磁卡開它。",
 				"", "",
-				"好大的寶箱啊，寶箱上有鎖頭打不開。"}
-	doorOpenMusic = {"sounds/opendoor.mp3", "sounds/openDoor2.mp3", "sounds/openDoor2.mp3", "sounds/opendoor.mp3",
+				"好大的寶箱啊，寶箱上有鎖頭打不開。","是小女孩口中的管家機器人，還有看起來是管家機器人專用的出入口。"}
+	doorOpenMusic = {"sounds/opendoor.mp3", "sounds/opendoor.mp3", "sounds/openDoor2.mp3", "sounds/opendoor.mp3",
 					 "sounds/opendoor.mp3", "sounds/opendoor.mp3", "sounds/opendoor.mp3", "sounds/opendoor.mp3", "sounds/opendoor.mp3", "sounds/opendoor.mp3",
 					 0,
 					 "sounds/opendoor.mp3",
-					 "sounds/openDoor2.mp3", "sounds/opendoor.mp3", "sounds/opendoor.mp3",
-					 "sounds/opendoor.mp3", "sounds/opendoor.mp3",
-					 0}
+					 "sounds/openDoor2.mp3", "sounds/opendoor.mp3", "sounds/openDoor2.mp3",
+					 "sounds/openDoor2.mp3", "sounds/opendoor.mp3",
+					 0,0}
 	doorUsedKeyMusic = {"sounds/keyOpenDoor.mp3", 0, "sounds/BB.mp3", 0,
 						0, "sounds/keyOpenDoor.mp3", "sounds/keyOpenDoor.mp3", 0, "sounds/keyOpenDoor.mp3", "sounds/keyOpenDoor.mp3",
 						"sounds/keyOpenDoor.mp3",
 						0,--1,
 						0, 0, "sounds/BB.mp3",
 						0, 0,
-						"sounds/keyOpenDoor.mp3"}
+						"sounds/keyOpenDoor.mp3",0}
 end
 function door:init(m, id, l, k, go, b)
 	self.lock = l
